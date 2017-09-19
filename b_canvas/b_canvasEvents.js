aab4e36fc4aef368781bd63469576c62096602d1
Juan Martin Barto Cerruti <juan.cerruti@educatina.com>
2017-09-19 18:01:41 -0300

    add scaled canvas click respect pos

--- b_canvas/b_canvasEvents.js
+++ b_canvas/b_canvasEvents.js
@@ -47,19 +47,38 @@ CanvasEvents = function(){
   }()
 
   ce_self.addClickEvent = function(canvas, ks){
-    canvas.onclick = function(event){
-      var rect = canvas.getBoundingClientRect();
-      var x = event.clientX - rect.left;
-      var y = event.clientY - rect.top;
-      for (var i = 0; i < ks.length; i++) {
-        if(typeof ks[i].clickInside == "function"){
-          if(( x > ks[i].drawPosX() && x < ks[i].drawPosX()+ks[i].width )
-          &&( y > ks[i].drawPosY() && y < ks[i].drawPosY() + ks[i].height )){
-            ks[i].clickInside();
+    this.config  = {
+      scaled: false,
+    }
+    this.setConfig = function(n,v){
+      this.config[n] = v
+      this.setCanvasClick(this.config)
+    }
+    this.setCanvasClick = function(config){
+      canvas.onclick = function(t){
+        return function(event){
+          var rect = canvas.getBoundingClientRect();
+          var x = event.clientX - rect.left;
+          var y = event.clientY - rect.top;
+          for (var i = 0; i < ks.length; i++) {
+            if(t.scaled){
+              var x = (canvas.width * event.clientX) / window.innerWidth;
+              var y = (canvas.height * event.clientY) / window.innerHeight;
+            }
+            if(typeof ks[i].clickInside == "function"){
+              if(( x > ks[i].drawPosX() && x < ks[i].drawPosX()+ks[i].width )
+              &&( y > ks[i].drawPosY() && y < ks[i].drawPosY() + ks[i].height )){
+                ks[i].clickInside();
+              }
+            }
           }
         }
-      }
+      }(config)
     }
+    this.setCanvasClick(this.config);
+
+
+    return this;
   }
 
 }
